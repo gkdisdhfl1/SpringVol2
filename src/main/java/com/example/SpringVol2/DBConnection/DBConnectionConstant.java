c50b050c6e417128f094a45ea3f8dbd951022986
gkdisdhfl1 <67906779+gkdisdhfl1@users.noreply.github.com>
2023-12-25 00:27:46 +0900

    Update DBConnectionConstant.java
    정보 삭제

--- src/main/java/com/example/SpringVol2/DBConnection/DBConnectionConstant.java
+++ src/main/java/com/example/SpringVol2/DBConnection/DBConnectionConstant.java
@@ -3,7 +3,7 @@ package com.example.SpringVol2.DBConnection;
 public class DBConnectionConstant {
     public static final String URL = "jdbc:mysql://localhost:3306/community3";
 
-    public static final String USERNAME = "root";
+    public static final String USERNAME = "";
 
-    public static final String PASSWORD = "rJadMsdHfL1)2(qp";
+    public static final String PASSWORD = "";
 }
