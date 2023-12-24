09018941786991adc92ff33169291308022f2105
심명교 <myeounggyo@gmail.com>
2023-12-25 00:14:05 +0900

    jdbc

---
+++ src/main/java/com/example/SpringVol2/DBConnection/DBConnectionConstant.java
@@ -0,0 +1,9 @@
+package com.example.SpringVol2.DBConnection;
+
+public class DBConnectionConstant {
+    public static final String URL = "jdbc:mysql://localhost:3306/community3";
+
+    public static final String USERNAME = "root";
+
+    public static final String PASSWORD = "rJadMsdHfL1)2(qp";
+}
