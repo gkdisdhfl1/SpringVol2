fd86306adcc579b779c643d828df56fbac9b794c
심명교 <myeounggyo@gmail.com>
2024-01-07 21:28:22 +0900

    로그인기능 구현과 jwt token 발행 추가

--- src/main/java/com/example/SpringVol2/Service/UserService.java
+++ src/main/java/com/example/SpringVol2/Service/UserService.java
@@ -1,5 +1,7 @@
 package com.example.SpringVol2.Service;
 
+import com.example.SpringVol2.domain.DTO.JoinRequest;
+import com.example.SpringVol2.domain.DTO.LoginRequest;
 import com.example.SpringVol2.domain.User;
 
 import java.util.List;
@@ -15,4 +17,12 @@ public interface UserService {
     User updateUser(String email, User user);
 
     int deleteUser(User user);
+
+    public boolean checkEmailDuplicate(String email);
+
+    public void join(JoinRequest req);
+
+    public User login(LoginRequest req);
+
+    public User getLoginUserByEmail(String email);
 }
